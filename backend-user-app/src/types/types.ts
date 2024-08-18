d516f6104b03fa33cdacb849874bcbe0d8874b57
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-08-18 19:35:35 +0530

    adding more types

--- backend-user-app/src/types/types.ts
+++ backend-user-app/src/types/types.ts
@@ -1,12 +1,18 @@
 import { z } from "zod";
 
-export const userSignup = z.object({
+export const SignupSchema = z.object({
     email:z.string().min(6),
     password:z.string().min(5),
     name:z.string().min(3)
 })
 
-export const userSignin = z.object({
+export const SigninSchema = z.object({
     email:z.string(),
     password:z.string(),
+})
+
+export const orderCreateSchema = z.object({
+    items: z.array(z.object({
+        item: z.string()
+    }))
 })
