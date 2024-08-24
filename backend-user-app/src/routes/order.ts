6eda0b40b27d9e9e8b47e87859d075b4c28614f9
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-08-24 23:37:15 +0530

    adding orders routes

--- backend-user-app/src/routes/order.ts
+++ backend-user-app/src/routes/order.ts
@@ -1,17 +1,75 @@
 import { Router } from "express";
+import { prismaClient } from "../db/db";
+import { orderCreateSchema } from "../types/types";
+import { authMiddleWare } from "../middleware";
 
 const router = Router();
 
-router.post("/",(req, res) => {
+router.post("/",authMiddleWare, async (req, res) => {
+    // @ts-ignore
+    const id = req.id;
+    const body = req.body;
+    const parsedData = orderCreateSchema.safeParse(body)
 
-})
+    if (!parsedData.success) {
+        return res.status(411).json({
+            message: "incorrect inputs"
+        })
+    }
 
-router.get("/",(req, res) => {
+    try {
+        const orderId = await prismaClient.order.create({
+            data:{
+                userId: id,
+                items:{
+                    create:parsedData.data.items.map((x) => ({
+                        name:x.item
+                    }))
+                }
+            }
+        }) 
+        
+        return res.json({
+            orderId
+        }).status(200)
+
+    } catch (error) {
+        console.log(error)
+    }
     
 })
 
-router.get("/:orderId",(req, res) => {
-    
+router.get("/", authMiddleWare, async (req, res) => {
+    // @ts-ignore
+    const id = req.id
+
+    const orders = await prismaClient.order.findMany({
+        where:{
+            userId:id
+        },
+        include:{
+            items:true
+        }
+    });
+
+    return res.json({
+        orders
+    })
+})
+
+router.get("/:orderId", authMiddleWare, async (req, res) => {
+    // @ts-ignore
+    const id = req.id
+    const orderId = req.params.orderId
+    const order = await prismaClient.order.findFirst({
+        where:{
+            id:orderId,
+            userId:id
+        },
+        include:{
+            items:true
+        }
+    }) 
 })
 
 export const orderRouter = router;
