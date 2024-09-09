fa750d0839ff12625bc13b8e53150396882fc30b
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-09-09 21:06:24 +0530

    adding pakages & basic cylinder

--- frontend/components/Circular_Canvas.tsx
+++ frontend/components/Circular_Canvas.tsx
@@ -1,11 +1,17 @@
 "use client"
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber"
 
 export const Circular_Canvas = () => {
-    return <Canvas>
-        <mesh>
-            <boxGeometry/>
-            <meshBasicMaterial/>
-        </mesh>
-    </Canvas>
+    return ( <div className="w-full h-full flex justify-center fixed">
+        <Canvas style={{width: "500px", height: "500px"}}>
+            <OrbitControls/>
+            <ambientLight />
+            <mesh>
+                <cylinderGeometry args={[1.5, 1.5, 1.5, 30, 30,]} />
+                <meshStandardMaterial/>
+            </mesh>
+        </Canvas>
+    </div>
+    );
 }
