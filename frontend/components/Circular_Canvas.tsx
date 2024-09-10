0e4146ef083c9fbd5db4b85c054f69bb92163ccc
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-09-10 23:43:01 +0530

    adding rotation

--- frontend/components/Circular_Canvas.tsx
+++ frontend/components/Circular_Canvas.tsx
@@ -5,8 +5,8 @@ import { Scene } from "./Scene";
 
 export const Circular_Canvas = () => {
 
-    return ( <div className="w-full h-full flex justify-center fixed">
-        <Canvas style={{width: "500px", height: "500px"}}>
+    return ( <div className="w-full h-full flex justify-center">
+        <Canvas camera={{fov: 25}} style={{width:"400px", height:"400px" }}>
             <OrbitControls/>
             <ambientLight />
             <Scene/>
--- frontend/components/Scene.tsx
+++ frontend/components/Scene.tsx
@@ -1,14 +1,21 @@
 import { useTexture } from "@react-three/drei";
-import React from "react";
+import { useFrame } from "@react-three/fiber";
+import React, { useRef } from "react";
 import * as THREE from "three"
 
 export const Scene = () => {
     let tex = useTexture("./image3.webp");
+    let cyl = useRef(null);
+    useFrame((state,delta) => {
+        cyl.current.rotation.y += delta;
+    });
 
     return (
-        <mesh>
-        <cylinderGeometry args={[1.5, 1.5, 1.5, 60, 30, true]} />
-        <meshStandardMaterial map={tex} side={THREE.DoubleSide} />
-    </mesh>
+        <group rotation={[0,1.4,0.5]}> 
+            <mesh ref={cyl} >
+                <cylinderGeometry args={[1, 1, 1, 60, 60, true]} />
+                <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
+            </mesh>
+        </group>
 )
 }
