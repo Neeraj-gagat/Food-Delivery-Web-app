4e7f3006ddbfad96e4d7ea242a1d638adf310696
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-09-13 19:02:58 +0530

    canvas up and running

--- frontend/components/Circular_Canvas.tsx
+++ frontend/components/Circular_Canvas.tsx
@@ -2,14 +2,25 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber"
 import { Scene } from "./Scene";
+import { Bloom, EffectComposer, ToneMapping } from "@react-three/postprocessing";
+
 
 export const Circular_Canvas = () => {
 
-    return ( <div className="w-full h-full flex justify-center">
-        <Canvas camera={{fov: 25}} style={{width:"400px", height:"400px" }}>
+    return ( <div className="w-screen h-screen flex justify-center">
+        <Canvas  style={{width:"800px", height:"600px" }}>
             <OrbitControls/>
             <ambientLight />
             <Scene/>
+            <EffectComposer > 
+            <Bloom
+                mipmapBlur
+                intensity={12.0} // The bloom intensity.
+                luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
+                luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
+                />
+                {/* <ToneMapping adaptive/> */}
+            </EffectComposer>          
         </Canvas>
     </div>
     );
--- frontend/components/Scene.tsx
+++ frontend/components/Scene.tsx
@@ -4,16 +4,20 @@ import React, { useRef } from "react";
 import * as THREE from "three"
 
 export const Scene = () => {
-    let tex = useTexture("./image3.webp");
+    let tex = useTexture("./image2.webp");
     let cyl = useRef(null);
+    
     useFrame((state,delta) => {
-        cyl.current.rotation.y += delta;
+        if (cyl.current) {
+            // @ts-ignore
+            cyl.current.rotation.y += delta;            
+        }
     });
 
     return (
         <group rotation={[0,1.4,0.5]}> 
             <mesh ref={cyl} >
-                <cylinderGeometry args={[1, 1, 1, 60, 60, true]} />
+                <cylinderGeometry args={[2, 2, 2, 60, 60, true]} />
                 <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
             </mesh>
         </group>
