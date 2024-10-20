a3cd51f6ed107728ba62b9eae46e585e6ed07b88
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-10-20 22:21:33 +0530

    fixes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 import { Circular_Canvas } from "@/components/Circular_Canvas";
 
 export default function Home() {
-  return <div className=" flex justify-center">
+  return <div className="bg-white w-full h-full flex justify-center">
     <Circular_Canvas></Circular_Canvas>
   </div>
 }
