9eaaa8f9793043dcad05cd9081c9f7e924af0482
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-10-26 23:48:27 +0530

    appbar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,9 @@
+import { Appbar } from "@/components/Appbar";
 import { Circular_Canvas } from "@/components/Circular_Canvas";
 
 export default function Home() {
   return <div className="bg-white w-full h-full flex justify-center">
+    <Appbar/>
     <Circular_Canvas></Circular_Canvas>
   </div>
 }
---
+++ frontend/components/Appbar.tsx
@@ -0,0 +1,11 @@
+
+export const Appbar = () => {
+    return <div className="flex border-b p-3 px-10">
+        <div className="flex justify-end">
+            <div>corporate</div>
+            <div>offers</div>
+            <div>Sign in</div>
+            <div>Cart</div>
+        </div>
+    </div>
+} 
