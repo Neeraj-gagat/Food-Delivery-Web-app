ab26a96a4478c1269296c25e462388b5839bbb38
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-11-10 23:58:25 +0530

    adding searchRoute

--- backend-user-app/src/index.ts
+++ backend-user-app/src/index.ts
@@ -2,6 +2,7 @@ import  express  from "express";
 import cors from "cors"
 import { userRouter } from "./routes/user";
 import { orderRouter } from "./routes/order";
+import { searchRouter } from "./routes/search";
 
 
 const app = express();
@@ -11,5 +12,7 @@ app.use(cors())
 app.use("/api/v1/user", userRouter);
 
 app.use("/api/v1/order", orderRouter);
+
+app.use("/api/v1/restaurants", searchRouter);
 
 app.listen(3000)
---
+++ backend-user-app/src/routes/search.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { authMiddleWare } from "../middleware";
+// 9779204812
+
+const router = Router();
+
+router.get("search",async(req,res) => {
+
+  const lat = req.query.lat || '40.7128';
+  const lon = req.query.lon || '-74.0060';
+  const radius = req.query.radius || '1000';
+  const query = `[out:json];node["amenity"="restaurant"](around:${radius},${lat},${lon});out;`;
+  const url = `https://overpass-api.de/api/interpreter?data=${encodeURIComponent(query)}`;
+   
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data.elements);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+export const searchRouter = router;
