3897eba07e63a6bf9b958fe51394c9c2b2f94009
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-11-29 21:32:27 +0530

    adding rider prisma schema & zod schema

--- backend-merchant-nd-jobs/src/types/type.ts
+++ backend-merchant-nd-jobs/src/types/type.ts
@@ -12,7 +12,18 @@ export const MerchantSigninSchema = z.object({
 })
 
 export const ItemSchema = z.object({
-    name: string().min(3),
-    image:string().min(5),
-    merchantId:number()
+    name: z.string().min(3),
+    image:z.string().min(5),
+    merchantId:z.number()
+})
+
+export const RiderSignupSchema = z.object({
+    email:z.string().min(6),
+    password:z.string().min(6),
+    name:z.string().min(3)
+})
+
+export const Ridersigninschema = z.object({
+    email:z.string(),
+    password:z.string()
 })
