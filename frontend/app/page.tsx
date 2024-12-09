278f66c7b8f70fa2610175f2c5731f42bddd6948
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-12-09 21:40:15 +0530

    changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,15 @@
 import { Appbar } from "@/components/Appbar";
 import { Circular_Canvas } from "@/components/Circular_Canvas";
+import { HeroSectionLanding } from "@/components/HeroSectionlanding";
 
 export default function Home() {
-  return <div className="bg-white w-full h-full flex justify-center">
+  return <div className="bg-white w-full h-full justify-center">
     <Appbar/>
-    {/* <Circular_Canvas></Circular_Canvas> */}
+    <div className="justify-center flex">
+      <div className="">
+    <Circular_Canvas></Circular_Canvas>
+    {/* <HeroSectionLanding></HeroSectionLanding> */}
+    </div>
+    </div>
   </div>
 }
---
+++ frontend/components/HeroSectionlanding.tsx
@@ -0,0 +1,8 @@
+
+export const HeroSectionLanding = () => {
+    return <div>
+        <div className="font-extrabold text-red-700 text-2xl justify-center">
+        Craving something delicious? We&apos;ve got it covered!
+        </div>
+    </div>
+}
