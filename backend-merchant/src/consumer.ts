78a96306c515e7b4b50626fc83fe25daa395098a
Neeraj-gagat <neerajgagat9999@gmail.com>
2025-01-02 19:25:18 +0530

    adding consumer for merchant

---
+++ backend-merchant/src/consumer.ts
@@ -0,0 +1,38 @@
+import RabbitMQ from "./rabbitmq/rabbitmq";
+
+const consumeMessages = async (
+    queue: string,
+    onMessage: (message: any) => void
+): Promise<void> => {
+    try {
+        const rabbitMq = RabbitMQ.getInstance();
+        await rabbitMq.connect();
+        const channel = rabbitMq.getChannel();
+
+        await channel.assertQueue(queue, { durable: true });
+
+        console.log(`Waiting for messages in ${queue}...`);
+
+        // settingup consumer
+        channel.consume(
+            queue,
+            (message) => {
+                if (message) {
+                    const content = JSON.parse(message.content.toString());
+                    console.log(`Received message from ${queue}:`, content);
+
+                    onMessage(content);
+
+                    // Acknowledging msg
+                    channel.ack(message);
+                }
+            },
+            { noAck: false } // Ensure manual acknowledgment
+        );
+    } catch (error) {
+        console.error(`Failed to consume messages from ${queue}:`, error);
+        throw error;
+    }
+};
+
+export default consumeMessages;
